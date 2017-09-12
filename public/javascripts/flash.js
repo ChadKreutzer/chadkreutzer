c6bc4accfaca8987e4a50c48caf6ffe19c6fc1d4
ChadKreutzer <kanthalion@gmail.com>
2017-09-12 16:34:37 -0600

    extract flash js to file

---
+++ public/javascripts/flash.js
@@ -0,0 +1,12 @@
+const closeButton = document.getElementById('close');
+const flashMsg = document.getElementsByClassName('flash')[0];
+let timeoutID;        
+
+const hideMsg = () => {
+    const removeMsg = () => flashMsg.setAttribute('style', 'display:none');
+    
+    flashMsg.classList.add('hidden');
+    timeoutID = setTimeout(removeMsg, 2000);
+}
+timeoutID = setTimeout(hideMsg, 5000);
+closeButton.addEventListener('click', hideMsg);
